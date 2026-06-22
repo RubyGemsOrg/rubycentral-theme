daa5a9933de9835096705cdc1029f13e71641ac6
Elaina Natario <elaina.natario@gmail.com>
2026-06-22 15:21:44 -0400

    use data attrs instead of classes

--- src/js/app/global-nav.js
+++ src/js/app/global-nav.js
@@ -1,12 +1,12 @@
-const nav = document.querySelector(".global-nav");
-const navToggle = document.querySelector(".global-nav__menu-toggle");
+const nav = document.querySelector("[data-global-nav]");
+const navToggle = document.querySelector("[data-global-nav-toggle]");
 
 function toggleNav() {
     const isOpen = nav.classList.contains("is-open");
 
     nav.classList.toggle("is-open", !isOpen);
     nav.classList.toggle("is-closed", isOpen);
-    navToggle.setAttribute("aria-expanded", String(!isOpen));
+    navToggle.setAttribute("aria-expanded", !isOpen);
 }
 
 navToggle.addEventListener("click", toggleNav);
